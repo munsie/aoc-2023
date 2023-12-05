2b49cb57ef8788cd8d55ffd7d23abeec2cbedd78
Dennis Munsie <dmunsie@gmail.com>
2023-12-05 12:08:19 -0500

    Added solution for day 5, part 1.  Added day 5, part 2.

---
+++ day05/seed.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+def read_range_map(f):
+    # read the header line
+    f.readline()
+
+    m = []
+    while True:
+        s = ' '.join(f.readline().split())
+        if s == '':
+            break
+        m.append([int(i) for i in s.split(' ')])
+
+    return m
+
+def find_in_range_map(m, v):
+    for r in m:
+        if v >= r[1] and v < (r[1] + r[2]):
+            return r[0] + v - r[1]
+    return v
+
+if __name__ == '__main__':
+
+    seeds = None
+    seed_to_soil_map = None
+    soil_to_fertilizer_map = None
+    fertilizer_to_water_map = None
+    water_to_light_map = None
+    light_to_temperature_map = None 
+    temperature_to_humidity_map = None
+    humidity_to_location_map = None
+
+    # load all of our input data
+    with open('input.txt') as f:
+        # read the list of seeds first
+        seeds = [int(i) for i in ' '.join(f.readline().split()).split(':')[1].split(' ')[1:]]
+        f.readline()
+
+        # load in the range maps
+        seed_to_soil_map = read_range_map(f)
+        soil_to_fertilizer_map = read_range_map(f)
+        fertilizer_to_water_map = read_range_map(f)
+        water_to_light_map = read_range_map(f)
+        light_to_temperature_map = read_range_map(f)
+        temperature_to_humidity_map = read_range_map(f)
+        humidity_to_location_map = read_range_map(f)
+
+    # for each seed, we need to find the location
+    lowest_seed = 0
+    lowest_location = 99999999999999999999
+
+    for seed in seeds:
+        soil = find_in_range_map(seed_to_soil_map, seed)
+        fertilizer = find_in_range_map(soil_to_fertilizer_map, soil)
+        water = find_in_range_map(fertilizer_to_water_map, fertilizer)
+        light = find_in_range_map(water_to_light_map, water)
+        temperature = find_in_range_map(light_to_temperature_map, light)
+        humidity = find_in_range_map(temperature_to_humidity_map, temperature)
+        location = find_in_range_map(humidity_to_location_map, humidity)
+
+        print(f'Seed {seed}, soil {soil}, fertilizer {fertilizer}, water {water}, light {light}, temperature {temperature}, humidity {humidity}, location {location}')
+        
+        if location < lowest_location:
+            lowest_location = location
+            lowest_seed = seed
+
+    print(f'lowest_location = {lowest_location}, seed = {lowest_seed}')
