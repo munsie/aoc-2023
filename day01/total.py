5167f5f9971162db227f94b9d168a0fc40e21637
Dennis Munsie <dmunsie@gmail.com>
2023-12-07 18:19:42 -0500

    Fixed day 1, part 1 code.

--- day01/total.py
+++ day01/total.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
 
-total = 0
+if __name__ == '__main__':
+    total = 0
 
-with open('input2.txt') as f:
-    for line in f:
-        first_digit = line[[x.isdigit() for x in line].index(True)]
+    with open('input.txt') as f:
+        for line in f:
+            first_digit = line[[x.isdigit() for x in line].index(True)]
        
-        rev_line = "".join(reversed(line))
-        last_digit = rev_line[[x.isdigit() for x in rev_line].index(True)]
+            rev_line = "".join(reversed(line))
+            last_digit = rev_line[[x.isdigit() for x in rev_line].index(True)]
 
-        line_value = first_digit + last_digit
-        total = total + int(line_value)
+            line_value = first_digit + last_digit
+            total += int(line_value)
 
-print(total)
+    print(total)
